04be0544484793fc91ef7d0cad6b13791f8ec9ae
Eneada Sulaj <addd.github@gmail.com>
2021-11-30 00:29:16 +0100

    update user profile

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django. contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import fields
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User # bcs whenever the form validates it creates a new user
         fields = ['username', 'email', 'password1', 'password2'] # the fields that are here will be the ones in our form, in the written order!!! 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email'] # let's the program know that we'll work with the username and password
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -19,4 +19,21 @@ def register(request):
 
 @login_required #this is a decorator - it adds functionality to an existing function, in this case requires login before accessing the profile page
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user) # request.post to pass the post data
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated.')
+            return redirect('profile') # if u're not redirected it will give the message "are u sure u want to reload" becase it's post data thet is or should be filled, or u should run another post request
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    #to pass the created forms to the template, use context 
+    context = {
+        'u_form': u_form, 
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
